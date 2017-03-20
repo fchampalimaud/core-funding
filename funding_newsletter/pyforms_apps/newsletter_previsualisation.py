d301f46471ca4aa925bfd18d7c35a9156aa10355
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2017-03-20 11:37:10 +0000

    .

--- funding_newsletter/pyforms_apps/newsletter_previsualisation.py
+++ funding_newsletter/pyforms_apps/newsletter_previsualisation.py
@@ -1,10 +1,13 @@
 from funding_opportunities_models.models import FundingOpportunity
 from pyforms_web.web.BaseWidget import BaseWidget
 from pyforms_web.web.Controls.ControlHtml import ControlHtml
+from pyforms_web.web.Controls.ControlEmail import ControlEmail
 from pyforms_web.web.Controls.ControlButton import ControlButton
 from orquestra.plugins import LayoutPositions
+from django.core.mail       import EmailMessage
 from django.utils import timezone
 from datetime import timedelta
+from django.conf import settings
 
 from funding_newsletter.render_newsletter import render_newsletter
 
@@ -20,15 +23,41 @@ class NewsletterPrevisualisation(BaseWidget):
 	layout_position = LayoutPositions.NEW_TAB
 	
 	def __init__(self):
-		super(NewsletterPrevisualisation, self).__init__('Newsletter pre-visualisation')
+		super(NewsletterPrevisualisation, self).__init__(self.label)
 
 		self._htmlcontrol = ControlHtml('Pre-visualisation')
-		self._refresh_btn = ControlButton('Refresh')
-		self.formset 	  = ['_refresh_btn', '_htmlcontrol']
+		self._email		  = ControlEmail('Email')
+		self._refresh_btn = ControlButton('<i class="refresh icon"></i> Refresh')
+		self._sendto_btn  = ControlButton('<i class="mail outline icon"></i> Sent to')
+		self.formset 	  = [
+			('_email', '_sendto_btn','_refresh_btn' ),
+			'_htmlcontrol'
+		]
 
 		self._refresh_btn.value = self.__refresh_event
+		self._sendto_btn.value  = self.__sendto_event
 
 		self._htmlcontrol.value = render_newsletter(False)
+		self._email.hide()
+
+	def __sendto_event(self):
+		if self._email.visible==False:
+			self._email.show()
+		else:
+			body = render_newsletter(False)
+			try:
+				msg = EmailMessage(
+					settings.FUNDING_OPPORTUNITIES_EMAIL_SUBJECT, 
+					body, 
+					settings.EMAIL_FROM, 
+					tuple(self._email.value) )
+				msg.content_subtype = "html"
+				msg.send()
+				self.success('Email sent with success','Success')
+				self._email.hide()
+			except Exception as e:
+				self.alert(str(e),'Error')	
+			
 
 	def __refresh_event(self):
 		body = render_newsletter(False)
